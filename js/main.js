55d8546b8afc531805c92f23c6e39c5ff5b17065
Никита <dolganovnik20032906@gmail.com>
2022-09-15 13:01:47 +0500

    Правки рандомного числа и файла index

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const randomNumber = (min, max) => {
   if(min < 0 || max < 0){
     return 0;
   }
-
-  
   const arrayNumbers = [min, max];
   if(min >= max){
     min = arrayNumbers[1];
@@ -12,5 +10,3 @@ const randomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 const lenghtChecked = (text , maxLenght) =>(String(text).length <= maxLenght);
-
-console.log(randomNumber(-1000,100));
