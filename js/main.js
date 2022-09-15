44a077f8e60c1a1d6e0422bfac5dd8de485b5be4
Никита <dolganovnik20032906@gmail.com>
2022-09-15 11:09:05 +0500

    Рандомное число и проверка строки на длину

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-function getSum(a,b){
-  const text = 'Сумма чисел равна:';
-  const sum = a + b;
-  const del = a/b;
-  return `${text  } ${  sum}`;
-}
-getSum(1,2);
+const randomNumber = (min, max) => {
+  if(min >= max){
+    const newMax = min;
+    min = max;
+    max = newMax;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+const lenghtCheck = (text , maxLenght) =>(String(text).length <= maxLenght);
+
+
+console.log(lenghtCheck('Абобыч Абобов Абоба', 10));
