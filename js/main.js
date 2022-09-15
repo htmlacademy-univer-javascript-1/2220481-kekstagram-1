822f7d7db9659ed057518094e9de64f090ab9693
Никита <dolganovnik20032906@gmail.com>
2022-09-15 12:58:41 +0500

    Правки рандомное число

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const randomNumber = (min, max) => {
+  if(min < 0 || max < 0){
+    return 0;
+  }
+
+  
+  const arrayNumbers = [min, max];
   if(min >= max){
-    const newMax = min;
-    min = max;
-    max = newMax;
+    min = arrayNumbers[1];
+    max = arrayNumbers[0];
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 const lenghtChecked = (text , maxLenght) =>(String(text).length <= maxLenght);
+
+console.log(randomNumber(-1000,100));
