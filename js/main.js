bd8ea87ee228a9bcea0965a996c908a79757732c
Никита <dolganovnik20032906@gmail.com>
2022-09-26 11:25:33 +0500

    Четвертое задание

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+const COUNT_OBJECT = 25;
+
 const NAME = [
   'Абоба',
   'Matthew Ball',
   'Алтынмаргендурдинпарденхуинбек'
 ];
+
 const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -12,15 +15,13 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const descriptions = [
+const DESCRIPTIONS = [
   'Фотокарточка',
   'случайный снимок',
   'Как вам фото с новой камеры?'
 ];
 
-const arrayObject = [];
-const countObject = 25;
-
+const arrayObjects = [];
 
 const randomNumber = (min, max) => {
   if(min < 0 || max < 0){
@@ -33,40 +34,31 @@ const randomNumber = (min, max) => {
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-const pushArraySplice = (array) => {
-  const number = array[randomNumber(1,array.length)];
-  array.splice(array.indexOf(number), 1);
-  return number;
-};
-const pushArray = (count) => {
-  const array = [];
-  for(let i = 1; i <= count; i++){
-    array.push(i);
-  }
-  return array;
-};
-const arrayId = pushArray(25);
-const arrayUrl = pushArray(25);
-const arrayIdComments = pushArray(1000);
+
 const commentsArray= (count) => {
   const array = [];
-  for(let i = 0; i<=count -1; i++){
-    array[i] = {
-      id: pushArraySplice(arrayIdComments),
+  for(let i = 0; i<count; i++){
+    array.push({
+      id: i,
       avatar: `img/avatar-${randomNumber(1,6)}.svg`,
       message: MESSAGES[randomNumber(0, MESSAGES.length- 1)],
       name: NAME[randomNumber(0,NAME.length - 1)]
-    };
+    });
   }
   return array;
 };
-for(let i = 0; i < countObject; i++){
-  arrayObject[i] = {
-    id: pushArraySplice(arrayId),
-    url: `photos/${pushArraySplice(arrayUrl)}.jpg`,
-    description: descriptions[randomNumber(0, descriptions.length - 1)],
-    likes: randomNumber(15, 200),
-    comments: commentsArray(2)
-  };
-}
+
+const addPhotos = () => {
+  for(let i = 0; i < COUNT_OBJECT; i++){
+    arrayObjects[i] = {
+      id: i,
+      url: `photos/${i + 1}.jpg`,
+      description: DESCRIPTIONS[randomNumber(0, DESCRIPTIONS.length - 1)],
+      likes: randomNumber(15, 200),
+      comments: commentsArray(randomNumber(0,2))
+    };
+  }
+};
+
+addPhotos();
 
